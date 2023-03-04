346df7733dbb8d7e145b97ee78ba8dc2ef3154c1
Kate <bozhko2906872020@gmail.com>
2023-03-04 15:42:18 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import {useState} from "react";
+import {data} from "./data";
 import './App.css';
 
 function App() {
+  const [showDogs, setShowDogs] = useState(0);
+  const{id, number, puppyName, gender, month, description, price, image} = data[showDogs];
+
+const nextDog = () => {
+setShowDogs(showDogs => {
+  showDogs++;
+  if(showDogs > data.length-1) {
+    showDogs=0
+  }
+  return showDogs;
+})
+}
+const previousDog = () => {
+setShowDogs((showDogs => {
+  showDogs--;
+  if(showDogs < 0) {
+    return data.length-1;
+  }
+  return showDogs
+}))
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="replace">
+        <h1>Puppy's SHOP</h1>
+        </div>
+        <div className="replace"> 
+        <h2>Find a friend!!</h2>
+      </div>
+<div className="replace">
+  <img src={image} width="320px" height="300px" alt="puppy"/>
+</div>
+<div className="cont">
+<div className="btn pr">
+  <button onClick={previousDog}>Previous</button>
+</div>
+<div className="btn nx">
+  <button onClick={nextDog}>Next</button>
+</div>
+</div>
+<div className="replace">
+<h3>Price: $ {price}</h3>
+</div>
+    <div className="replace">
+      <h1>{number} - {puppyName}</h1>
+    </div>
+<div className="replace">
+<p>Gender: {gender}, age: {month}</p>
+</div>
+<div className="replace">
+<p>{description}</p>
+</div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const data = [
+
+    {
+        id: 1,
+        number: 1,
+        puppyName: "Lola",
+        gender: "female",
+        month: "three months",
+        description: "Lola is a very cute girl who need an attention, care and love!!",
+        price: 300,
+        image: "https://img.freepik.com/free-photo/adorable-beagle-puppy-solo-portrait_53876-64817.jpg?w=740&t=st=1673449795~exp=1673450395~hmac=ab95e98c226b7145e3a207ceb8bef12b35dd7837102501cb59a6d01392d5af14"
+    },
+
+    {
+    id: 2,
+    number: 2,
+    puppyName: "Nick",
+    gender: "male",
+    month: "four months",
+    description: "Very active boy is loking for a family!!",
+    price: 350,
+    image: "https://img.freepik.com/free-photo/beagles-puppies-looking-something_1150-18193.jpg?w=1060&t=st=1673445541~exp=1673446141~hmac=9f8a2e4d0235f083a5573f2a779aecf834b4e003500e5e257c62c3f0912e75b6"
+},
+
+{
+    id: 3,
+    number: 3,
+    puppyName: "Jack",
+    gender: "male",
+    month: "five months",
+    description: "Jack is looking for new mom and dad, likes an ative games!!!",
+    price: 250,
+    image: "https://img.freepik.com/free-photo/portrait-funny-carried-cute-puppy-siberian-husky-dog-standing-against-silver-plate-with-water-food-little-funny-dog-with-blue-eyes-gray-black-fur_7502-5602.jpg?w=1060&t=st=1673445966~exp=1673446566~hmac=9e74d3203d89db7467a09f964ad57cb6cf8fbf4ea8ac1dc2a4b56cc3a7801eaf"
+},
+{
+    id: 4,
+    number: 4,
+    puppyName: "Nick",
+    gender: "male",
+    month: "three months",
+    description: "Nick likes lyinhg on the sofa and need quiet family without kids",
+    price: 300,
+    image: "https://img.freepik.com/free-photo/studio-shot-papillon-fallen-little-dog-isolated-white_155003-44649.jpg?w=1060&t=st=1673451443~exp=1673452043~hmac=9d6a69ee8d55fae575fd3cea8365ee8c4c0d3b77ac37cc2f53b6d8d252a12778"
+},
+{
+    id: 5,
+    number: 5,
+    puppyName: "Mary",
+    gender: "female",
+    month: "three months",
+    description: "Mary is a very cute little girl who needs love and care",
+    price: 300,
+    image: "https://img.freepik.com/free-photo/cute-beige-shih-poo-maltipoo-dog-walking-wooden-deck_181624-36130.jpg?w=1060&t=st=1673446375~exp=1673446975~hmac=4d263724f01a805033dd4b9812f73b674bd2a949827f8486815befc831355851"
+},
+
+{
+    id: 6,
+    number: 6,
+    puppyName: "Rick",
+    gender: "male",
+    month: "four months",
+    description: "Rick absolutely loves children and active games!!",
+    price: 350,
+    image: "https://img.freepik.com/free-photo/adorable-white-little-puppy-isolated-blue_23-2148985899.jpg?w=1060&t=st=1673446549~exp=1673447149~hmac=4ad0fef9f0e6b2665811d7b5a4b0f9c4840102d7d6817143f24e0c0a62574910"
+},
+{
+    id: 7,
+    number: 7,
+    puppyName: "Fiona",
+    gender: "female",
+    month: "five months",
+    description: "Fiona likes tasty food and long walks",
+    price: 250,
+    image: "https://img.freepik.com/free-photo/run-maltipu-little-dog-is-posing_155003-22631.jpg?w=1060&t=st=1673451373~exp=1673451973~hmac=f9e7186b966a9db0d745bdbf3af4869ef20480c832a2b2127ce6a8c66d6b08a3"
+}
+]
